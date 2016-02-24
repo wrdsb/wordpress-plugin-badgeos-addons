5ec2c5978fbd38555a7cc879e11dd780af3427c6
James Schumann <jschumann@gmail.com>
2016-02-24 09:22:46 -0500

    Create wordpress-plugin-badgeos-addons.php

---
+++ wordpress-plugin-badgeos-addons.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Plugin Name: BadgeOS Addons from WRDSB
+* Plugin URI: https://github.com/wrdsb/wordpress-plugin-badgeos-addons
+* Description: Our own customizations for BadgeOS.
+* Author: WRDSB
+* Author URI: https://github.com/wordpress-plugin-badgeos-addons
+* Version: 0.0.1
+* License: GPLv3 or later
+* GitHub Plugin URI: wrdsb/wordpress-plugin-badgeos-addons
+* GitHub Branch: master
+*/
+
+require_once dirname( __FILE__ ) . "/achievement_categories.php";
+require_once dirname( __FILE__ ) . "/achievement_tags.php";
